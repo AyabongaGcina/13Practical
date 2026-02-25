f59b089733acfd8f4f2e49300c0421390a7a5b33
AyabongaGcina <4446494@myuwc.ac.za>
2026-02-25 14:23:51 +0000

    gg

--- test.java
+++ test.java
@@ -3,11 +3,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Arrays;
 import java.text.DecimalFormat;
 //Creating code
-public class timeMethods {
+public class test {
     public static int N = 32654; // Maximum possible key range
     public static Node[] records; // Array to store all records
     public static Node[] sortedRecords; // Sorted copy for binary search
